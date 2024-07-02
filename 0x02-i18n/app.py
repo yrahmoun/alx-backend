51cddb9734d343831aa446a1fb2863186b34ccd4
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-02 13:09:22 +0000

    task 8

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+task 8 module
+"""
+import pytz
+import datetime
+from typing import Dict, Union
+from flask import Flask
+from flask import g, request
+from flask import render_template
+from flask_babel import Babel
+from flask_babel import format_datetime
+
+
+class Config(object):
+    """configuration class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """Validate user login details"""
+    return users.get(int(id), None)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Gets locale from request object"""
+    options = [
+        request.args.get('locale', '').strip(),
+        g.user.get('locale', None) if g.user else None,
+        request.accept_languages.best_match(app.config['LANGUAGES']),
+        Config.BABEL_DEFAULT_LOCALE
+    ]
+    for locale in options:
+        if locale and locale in Config.LANGUAGES:
+            return locale
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """Get the timezone"""
+    tz = request.args.get('timezone', '').strip()
+    if not tz and g.user:
+        tz = g.user['timezone']
+    try:
+        tz = pytz.timezone(tz).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        tz = app.config['BABEL_DEFAULT_TIMEZONE']
+    return tz
+
+
+@app.before_request
+def before_request() -> None:
+    """Adds valid user to the global session object"""
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+    setattr(g, 'time', format_datetime(datetime.datetime.now()))
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Renders an html template"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
