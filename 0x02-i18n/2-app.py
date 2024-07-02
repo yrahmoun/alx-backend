048f456fb56ae70c4048bc34c6aac80f65625660
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-02 11:32:58 +0000

    task 3

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+task 2 module
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    """configuration class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Gets locale from request object"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Renders an html template"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
