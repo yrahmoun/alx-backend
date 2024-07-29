d2ecf962f4a36d11e5a4775aa2354924260c9799
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-29 11:30:52 +0000

    task 5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,25 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const listener = (message) => console.log(message);
+
+client.SUBSCRIBE('holberton school channel');
+
+client.on('message', (channel, message) => {
+  if (channel === 'holberton school channel') {
+    if (message === 'KILL_SERVER') {
+      client.UNSUBSCRIBE();
+      client.QUIT();
+    }
+    listener(message);
+  }
+});
